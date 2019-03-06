dc5bbd129cdfe96db531f6d77ab0f1c5d6dfe9ad
Vlad Tudorache <tudorache.vlad@gmail.com>
2019-03-06 19:43:20 +0100

    Update model.py

--- code/model.py
+++ code/model.py
@@ -3,5 +3,29 @@ import datetime
 Date = datetime.date
 
 class Patient(object):
-    def __init__(self, name, sex, birthday):
+    
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) \
+            and self.name == other.name \
+            and self.gender == other.gender \
+            and self.birthday == other.birthday
+    
+    def __gt__(self, other):
+        if not isinstance(other, self.__class__):
+            return NotImplemented
+        return self.birthday < other.birthday
+    
+    def __init__(self, name, gender, birthday):
+        super(Patient, self).__init__()
+        self.name = name
+        self.gender = gender
+        self.birthday = birthday
+   
+    def __repr__(self):
+        return '%s.%s(%r, %r, %r)' % (self.__class__.__module__, \
+                                      self.__class__.__name__, \
+                                      self.name, self.gender, self.birthday)
+    
+    @property
+    def age(self):
         pass
