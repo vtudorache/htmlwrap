8f2d196da04e1887547003a86cb9042715c45da8
Vlad Tudorache <tudorache.vlad@gmail.com>
2019-03-07 15:04:50 +0100

    Update model.py

--- code/model.py
+++ code/model.py
@@ -3,3 +3,14 @@ import datetime
 
 Date = datetime.date
 
+class FileWrapper(object):
+    
+    def __init__(self, filelike, length=0, blocksize=1024):
+        self.filelike = filelike
+        self.length = length
+        self.blocksize = blocksize
+        self.count = 0
+        
+    def __iter__(self):
+        return self
+    
