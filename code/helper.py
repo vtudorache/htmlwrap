3679c4167b319035c72a56c94322e4ea8d084770
Vlad Tudorache <tudorache.vlad@gmail.com>
2019-03-07 15:22:22 +0100

    Update and rename model.py to helper.py

---
+++ code/helper.py
@@ -0,0 +1,27 @@
+class FileWrapper(object):
+    
+    def __init__(self, filelike, length=0, blocksize=1024):
+        self.filelike = filelike
+        self.length = length
+        self.blocksize = blocksize
+        self.count = 0
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if not (self.filelike is None):
+            blocksize = self.length - self.count
+            if (self.length == 0) or (blocksize > self.blocksize):
+                blocksize = self.blocksize
+            if blocksize > 0:
+                content = self.filelike.read(blocksize)
+                self.count += len(content)
+                return content
+        raise StopIteration
+    
+    def close(self):
+        if hasattr(self.filelike, 'close'):
+            self.filelike.close()
+        self.filelike = None
+   
