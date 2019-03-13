11bd3806b3d069587ebcebcf588bbd179326f550
vtudorache <tudorache.vlad@gmail.com>
2019-03-13 22:55:46 +0100

    modified:   view.py

--- code/view.py
+++ code/view.py
@@ -58,34 +58,3 @@ class HTMLWrapper(object):
             if self._closing_tag:
                 parts.append(self._closing_tag)
         return separator.join(parts)
-
-class TableWrapper(HTMLWrapper):
-    
-    def __init__(self, attributes=None, compact=False, indent=None):
-        tag = 'table'
-        if attributes:
-            tag = '%s %s' % (tag, attributes)
-        super(TableWrapper, self).__init__(tag, compact, indent)
-        self.attributes = attributes
-        
-    def __call__(self, records, labels=None):
-        headers = []
-        keys = tuple(records[0].keys())
-        # callables must be created here to take in account changes of the
-        # instance attributes
-        tr = HTMLWrapper('tr', self.compact, self.indent)
-        th = HTMLWrapper('th')
-        td = HTMLWrapper('td')
-        thead = HTMLWrapper('thead', self.compact, self.indent)
-        tbody = HTMLWrapper('tbody', self.compact, self.indent)
-        for (i, s) in enumerate(keys):
-            if labels and (i < len(labels)) and labels[i]:
-                s = labels[i]
-            headers.append(th(s, True, True))
-        headers = thead(tr('\n'.join(headers)))
-        datarows = []
-        for r in records:
-            datarows.append(tr('\n'.join(td(r[k], True, True) \
-                for k in keys)))
-        datarows = tbody('\n'.join(datarows))
-        return super(TableWrapper, self).__call__('%s\n%s' % (headers, datarows))
